3bbd7f03fbefec1663ad128d4130f46067408bb6
Diamond <51243864+simondadiamond@users.noreply.github.com>
2025-05-05 21:24:56 -0400

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { DashboardPage } from './pages/DashboardPage';
+import { LoginPage } from './pages/LoginPage';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/login" element={<LoginPage />} />
         {/* Add other routes as needed */}
       </Routes>
     </Router>
